aa3f19f574c3b1b806f3e6d6467d20c0752ee116
Emil Kampp <100700894+av-emil@users.noreply.github.com>
2024-08-02 06:37:00 +0200

    Allow openapi_api_key to be configurable outside the env var

--- lib/i18n-ai/configuration.rb
+++ lib/i18n-ai/configuration.rb
@@ -2,11 +2,12 @@
 
 module I18nAi
   class Configuration
-    attr_accessor :generate_locales, :source_locale
+    attr_accessor :generate_locales, :source_locale, :openai_api_key
 
     def initialize
       @generate_locales = [:es]
       @source_locale = :en
+      @openai_api_key = ENV["OPENAI_API_KEY"].presence
     end
   end
 end
